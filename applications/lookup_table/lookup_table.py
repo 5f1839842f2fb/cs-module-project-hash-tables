7ca08803736d7b91b9c6f8e378a101ef9a581172
5f1839842f2fb <rewt@DESKTOP-EE2A1I3.localdomain>
2020-09-09 01:45:07 -0400

    expensive seq, lookup, word count

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,5 +1,6 @@
 # Your code here
-
+import random
+import math
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -9,13 +10,19 @@ def slowfun_too_slow(x, y):
 
     return v
 
+cache = {}
+for a in range(2, 14):
+        for b in range(3, 6):
+            cache[(a, b)] = slowfun_too_slow(a, b)
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
-
+    
+    return cache[(x, y)]
 
 
 # Do not modify below this line!
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,7 +1,22 @@
+import string
+
 def word_count(s):
     # Your code here
-
-
+    s = s.replace("\n", " ")
+    s = s.replace("\r", " ")
+    s = s.replace("\t", " ") # surely there's a better way to do this
+    wordList = s.lower().split(" ")
+    wordDict = {}
+    if len(s) > 0:
+        for element in wordList:
+            stripped = element.strip(string.punctuation)
+            if len(stripped) > 0:
+                if stripped in wordDict:
+                    wordDict[stripped] += 1
+                else:
+                    wordDict[stripped] = 1
+            
+    return wordDict
 
 if __name__ == "__main__":
     print(word_count(""))
